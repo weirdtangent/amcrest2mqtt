34b3c9192728f907b6891c533c4534f19300209f
Jeff Culverhouse <jeff@graystorm.com>
2025-02-24 16:59:17 -0500

    fix env var name

--- amcrest2mqtt.py
+++ amcrest2mqtt.py
@@ -458,7 +458,7 @@ else:
             'password': os.getenv("MQTT_PASSWORD"),  # can be None
             'qos': int(os.getenv("MQTT_QOS") or 0),
             'prefix': os.getenv("MQTT_PREFIX") or 'govee2mqtt',
-            'home_assistant_prefix': os.getenv("HOME_ASSISTANT_PREFIX") or "homeassistant",
+            'home_assistant_prefix': os.getenv("MQTT_HOME_ASSISTANT_PREFIX") or "homeassistant",
             'tls_enabled': os.getenv("MQTT_TLS_ENABLED") == "true",
             'tls_ca_cert': os.getenv("MQTT_TLS_CA_CERT"),
             'tls_cert': os.getenv("MQTT_TLS_CERT"),
