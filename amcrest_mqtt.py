53ea515f005e0830dcd45e9e8bca6f7859fdc760
Jeff Culverhouse <jeff@graystorm.com>
2025-10-09 16:21:11 -0400

    fix: tls_set call for ssl mqtt connections

--- amcrest_mqtt.py
+++ amcrest_mqtt.py
@@ -167,7 +167,7 @@ class AmcrestMqtt(object):
         )
 
         if self.mqtt_config.get('tls_enabled'):
-            self.mqttcnt.tls_set(
+            self.mqttc.tls_set(
                 ca_certs=self.mqtt_config.get('tls_ca_cert'),
                 certfile=self.mqtt_config.get('tls_cert'),
                 keyfile=self.mqtt_config.get('tls_key'),
